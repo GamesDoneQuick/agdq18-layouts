d9db716d659a44fcbe5b9192bc0b749e7af11ab9
Alex Van Camp <vancamp.alex@gmail.com>
2017-11-29 14:45:22 -0600

    perf(gdq-tronlines): use pool of nodes

--- graphics/elements/gdq-tronlines/gdq-tronlines.js
+++ graphics/elements/gdq-tronlines/gdq-tronlines.js
@@ -168,11 +168,29 @@ class GdqTronlines extends Polymer.Element {
 				}
 			},
 
+			debug: {
+				type: Boolean,
+				reflectToAttribute: true,
+				value: false
+			},
+
 			/**
-			 * An array containing all currently active nodes.
+			 * An array containing all nodes currently being drawn to the stage.
 			 */
-			nodes: {
+			_allocatedNodes: {
 				type: Array,
+				readOnly: true,
+				value() {
+					return [];
+				}
+			},
+
+			/**
+			 * An array containing all nodes currently unallocated.
+			 */
+			_freeNodes: {
+				type: Array,
+				readOnly: true,
 				value() {
 					return [];
 				}
@@ -190,6 +208,14 @@ class GdqTronlines extends Polymer.Element {
 		return Random.real(min, max, true)(Random.engines.browserCrypto);
 	}
 
+	static get BLOCK_SIZE() {
+		return 50;
+	}
+
+	static get WARNING_THRESHOLD() {
+		return 500;
+	}
+
 	ready() {
 		super.ready();
 
@@ -197,11 +223,20 @@ class GdqTronlines extends Polymer.Element {
 		const stage = new createjs.Stage(this.$.canvas);
 		createjs.Ticker.on('tick', () => {
 			this.advanceSimulation();
-			this.removeInvisibleNodes();
+			this._freeInvisibleNodes();
 
-			if (this.nodes.length > 1000) {
+			if (this.debug) {
+				const totalNodes = this._allocatedNodes.length + this._freeNodes.length;
+				this.$.debugInfo.textContent = `${this._allocatedNodes.length}/${totalNodes}`;
+			}
+
+			if (this._allocatedNodes.length > GdqTronlines.WARNING_THRESHOLD) {
 				if (!warnedLeak) {
-					console.warn('More than 1000 nodes are active, this is probably a leak!', this);
+					console.warn(
+						'More than %d nodes are active, this is probably a leak!',
+						GdqTronlines.WARNING_THRESHOLD,
+						this
+					);
 					warnedLeak = true;
 				}
 			} else {
@@ -221,15 +256,66 @@ class GdqTronlines extends Polymer.Element {
 	}
 
 	/**
+	 * Advances the simulation by one tick.
+	 * In most cases, this means one frame in a 60fps simulation.
+	 * @returns {undefined}
+	 */
+	advanceSimulation() {
+		const opacityRange = Math.abs(this.opacityStart - this.opacityEnd);
+		this._allocatedNodes.forEach(node => {
+			node.y -= node.speed;
+
+			const journeyPercentage = 1 - (node.y / (this._invertDimensions ? this.width : this.height));
+			node.alpha = this.opacityStart - (opacityRange * journeyPercentage);
+		});
+	}
+
+	_creationRateChanged(newVal) {
+		if (this._creationInterval) {
+			clearInterval(this._creationInterval);
+		}
+
+		this._creationInterval = setInterval(() => {
+			if (this._freeNodes.length <= 0) {
+				this._createBlockOfFreeNodes(GdqTronlines.BLOCK_SIZE);
+			}
+			const node = this._freeNodes.pop();
+			this._allocateNode(node);
+		}, (1000 / newVal));
+	}
+
+	/**
+	 * Creates and adds a block of new nodes to the _freeNodes array.
+	 * @param {Number} blockSize - The number of nodes to add.
+	 * @private
+	 * @returns {undefined}
+	 */
+	_createBlockOfFreeNodes(blockSize) {
+		for (let i = 0; i < blockSize; i++) {
+			this._freeNodes.push(this._createNode());
+		}
+	}
+
+	/**
 	 * Creates a new node instance.
-	 * Does not automatically add it to the stage.
+	 * @private
 	 * @returns {createjs.Shape} - The created node instance.
 	 */
-	createNode() {
-		const node = new createjs.Shape();
+	_createNode() {
+		return new createjs.Shape();
+	}
+
+	/**
+	 * Adds a node to the stage.
+	 * @param {createjs.Shape} node - The node to add to the stage.
+	 * @private
+	 * @returns {undefined}
+	 */
+	_allocateNode(node) {
 		const tailLength = GdqTronlines.getRandomReal(this.minTailLength, this.maxTailLength);
 		const tailEndColor = createjs.Graphics.getRGB(this.tailColor, 0.2);
 		const firstGradientStop = Math.min(this.nodeSize / this.tailLength, 1);
+
 		node.graphics
 			.beginLinearGradientFill([this.tailColor, tailEndColor], [firstGradientStop, 1], 0, 0, 0, tailLength)
 			.drawRect(0, 0, this.nodeSize, tailLength)
@@ -241,50 +327,28 @@ class GdqTronlines extends Polymer.Element {
 		node.alpha = this.opacityStart;
 		node.y = this._invertDimensions ? this.width : this.height;
 		node.x = GdqTronlines.getRandomReal(0, this._invertDimensions ? this.height : this.width);
-		return node;
-	}
 
-	/**
-	 * Adds a node to the stage.
-	 * @param {createjs.DisplayObject} node - The node to add to the stage.
-	 * @returns {undefined}
-	 */
-	dispatchNode(node) {
 		this.stage.addChild(node);
-		this.push('nodes', node);
-	}
-
-	/**
-	 * Advances the simulation by one tick.
-	 * In most cases, this means one frame in a 60fps simulation.
-	 * @returns {undefined}
-	 */
-	advanceSimulation() {
-		const opacityRange = Math.abs(this.opacityStart - this.opacityEnd);
-		this.nodes.forEach(node => {
-			node.y -= node.speed;
-
-			const journeyPercentage = 1 - (node.y / (this._invertDimensions ? this.width : this.height));
-			node.alpha = this.opacityStart - (opacityRange * journeyPercentage);
-		});
+		this._allocatedNodes.push(node);
 	}
 
 	/**
 	 * Removes all invisible nodes by removing them from both
 	 * the this.nodes array and this.stage.
+	 * @private
 	 * @returns {undefined}
 	 */
-	removeInvisibleNodes() {
-		this.nodes = this.nodes.filter(node => {
+	_freeInvisibleNodes() {
+		this._allocatedNodes = this._allocatedNodes.filter((node, index) => {
 			// If a node's alpha is less than zero, remove it.
 			if (node.alpha <= 0) {
-				this.removeNode(node);
+				this._freeNode(node, index);
 				return false;
 			}
 
 			// If a node has completely scrolled off the canvas, remove it.
 			if ((node.y + node.tailLength) <= 0) {
-				this.removeNode(node);
+				this._freeNode(node, index);
 				return false;
 			}
 
@@ -293,19 +357,17 @@ class GdqTronlines extends Polymer.Element {
 		});
 	}
 
-	removeNode(node) {
+	/**
+	 * Frees a node, removing it from the stage and returning it to the pool.
+	 * @param {createjs.DisplayObject} node - The node to free.
+	 * @param {Number} index - The index node to free in the _allocatedNodes array.
+	 * @private
+	 * @returns {undefined}
+	 */
+	_freeNode(node, index) {
+		this._allocatedNodes.splice(index, 1);
+		this._freeNodes.push(node);
 		this.stage.removeChild(node);
-	}
-
-	_creationRateChanged(newVal) {
-		if (this._creationInterval) {
-			clearInterval(this._creationInterval);
-		}
-
-		this._creationInterval = setInterval(() => {
-			const node = this.createNode();
-			this.dispatchNode(node);
-		}, (1000 / newVal));
 	}
 
 	_computeMinSpeed(speed, randomness) {
