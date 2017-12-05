b7f039db6563036818c12e1e42d9b05e50cf1222
Alex Van Camp <vancamp.alex@gmail.com>
2017-12-05 13:58:50 -0600

    feat(gdq-nameplates): implement FUI-style design (#1)

---
+++ graphics/elements/molecules/gdq-nameplate/gdq-nameplate-audio-indicator.js
@@ -0,0 +1,85 @@
+/**
+ * @customElement
+ * @polymer
+ */
+class GdqNameplateAudioIndicator extends Polymer.Element {
+	static get is() {
+		return 'gdq-nameplate-audio-indicator';
+	}
+
+	static get properties() {
+		return {
+			showing: {
+				type: Boolean,
+				observer: '_showingChanged'
+			},
+			vertPos: {
+				type: String,
+				reflectToAttribute: true,
+				value: 'top'
+			},
+			horizPos: {
+				type: String,
+				reflectToAttribute: true,
+				value: 'left'
+			},
+			animationDuration: {
+				type: Number,
+				value: 0.25
+			},
+			_maskProxy: {
+				type: Array,
+				readOnly: true,
+				value() {
+					return [-10, -10, 0];
+				}
+			}
+		};
+	}
+
+	ready() {
+		super.ready();
+		this.$.body.style.webkitMaskImage = `linear-gradient(
+			to right,
+			rgba(0,0,0,1) ${this._maskProxy[0]}%,
+			rgba(0,0,0,1) ${this._maskProxy[1]}%,
+			rgba(0,0,0,0) ${this._maskProxy[2]}%
+		)`;
+	}
+
+	show() {
+		return this._animateMask(100, 100, 110);
+	}
+
+	hide() {
+		return this._animateMask(-10, -10, 0);
+	}
+
+	_animateMask(stopOne, stopTwo, stopThree) {
+		return TweenLite.to(this._maskProxy, this.animationDuration, {
+			0: stopOne,
+			1: stopTwo,
+			2: stopThree,
+			ease: Power3.easeOut,
+			callbackScope: this,
+			onUpdate() {
+				this.$.body.style.webkitMaskImage = `linear-gradient(
+					to right,
+					rgba(0,0,0,1) ${this._maskProxy[0]}%,
+					rgba(0,0,0,1) ${this._maskProxy[1]}%,
+					rgba(0,0,0,0) ${this._maskProxy[2]}%
+				)`;
+			}
+		});
+	}
+
+	_showingChanged(newVal) {
+		if (newVal) {
+			return this.show();
+		}
+
+		return this.hide();
+	}
+}
+
+customElements.define(GdqNameplateAudioIndicator.is, GdqNameplateAudioIndicator);
---
+++ graphics/elements/molecules/gdq-nameplate/gdq-nameplate-result.js
@@ -0,0 +1,92 @@
+/**
+ * @customElement
+ * @polymer
+ */
+class GdqNameplateResult extends Polymer.Element {
+	static get is() {
+		return 'gdq-nameplate-result';
+	}
+
+	static get properties() {
+		return {
+			showing: {
+				type: Boolean,
+				observer: '_showingChanged'
+			},
+			side: {
+				type: String,
+				reflectToAttribute: true
+			},
+			place: Number,
+			time: String,
+			firstPlace: {
+				type: Boolean,
+				reflectToAttribute: true
+			},
+			lastPlace: {
+				type: Boolean,
+				reflectToAttribute: true
+			},
+			_tl: {
+				type: TimelineLite,
+				value() {
+					return new TimelineLite({autoRemoveChildren: true});
+				}
+			}
+		};
+	}
+
+	ready() {
+		super.ready();
+		TweenLite.set(this, {x: 0});
+		TweenLite.set(this.$.cover, {scaleX: 1});
+		TweenLite.set(this.$.place, {scaleX: 0});
+	}
+
+	show() {
+		const anim = new TimelineLite();
+		anim.to(this, 0.5, {
+			x: this.side === 'left' ? '-100%' : '100%',
+			ease: Power3.easeIn
+		});
+
+		anim.to(this.$.cover, 0.5, {
+			scaleX: 0,
+			ease: Power3.easeOut
+		});
+
+		anim.to(this.$.place, 0.182, {
+			scaleX: 1,
+			ease: Sine.easeOut
+		});
+
+		return anim;
+	}
+
+	hide() {
+		const anim = new TimelineLite();
+		anim.to(this.$.place, 0.182, {
+			scaleX: 0,
+			ease: Sine.easeIn
+		});
+
+		anim.to(this.$.cover, 0.5, {
+			scaleX: 1,
+			ease: Power3.easeIn
+		});
+
+		anim.to(this, 0.5, {
+			x: '0%',
+			ease: Power3.easeOut
+		});
+
+		return anim;
+	}
+
+	_showingChanged(newVal) {
+		const anim = newVal ? this.show() : this.hide();
+		this._tl.add(anim);
+	}
+}
+
+customElements.define(GdqNameplateResult.is, GdqNameplateResult);
