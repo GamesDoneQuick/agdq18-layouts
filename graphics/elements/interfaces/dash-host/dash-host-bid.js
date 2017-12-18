75229784387d20aa9142963396c556dbeae6656e
Alex Van Camp <vancamp.alex@gmail.com>
2017-12-18 17:16:32 -0600

    refactor(dash-host-bid): create dash-host-bid-option element

--- graphics/elements/interfaces/dash-host/dash-host-bid.js
+++ graphics/elements/interfaces/dash-host/dash-host-bid.js
@@ -41,20 +41,6 @@ class DashHostBid extends Polymer.MutableData(Polymer.Element) {
 		return options.slice(0, 3);
 	}
 
-	calcOptionMeterFillStyle(bid, option) {
-		if (!option || !bid.options || bid.options.length <= 0) {
-			return '';
-		}
-
-		let percent = option.rawTotal / bid.options[0].rawTotal;
-		percent = Math.max(percent, 0); // Clamp to min 0
-		percent = Math.min(percent, 1); // Clamp to max 1
-		if (Number.isNaN(percent)) {
-			percent = 0;
-		}
-		return `transform: scaleX(${percent});`;
-	}
-
 	bidHasMoreThanThreeOptions(bid) {
 		if (!bid.options) {
 			return false;
