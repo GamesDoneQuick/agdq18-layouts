4d1d0a4d7e8a56b9068d4c3b89c7bceb0564ec3f
Chris Hanel <chrishanel@gmail.com>
2017-12-19 01:06:53 -0800

    [layouts] atom-gridlines, first layout implementations

---
+++ graphics/elements/atoms/atom-gridlines/atom-gridlines.js
@@ -0,0 +1,11 @@
+/**
+ * @customElement
+ * @polymer
+ */
+class AtomGridlines extends Polymer.Element {
+	static get is() {
+		return 'atom-gridlines';
+	}
+}
+
+customElements.define(AtomGridlines.is, AtomGridlines);
