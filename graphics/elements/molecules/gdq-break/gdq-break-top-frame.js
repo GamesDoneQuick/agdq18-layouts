28ba7eac085b0b71d0505c6dcacea9e51bc2b05c
Alex Van Camp <vancamp.alex@gmail.com>
2017-12-23 16:02:04 -0600

    feat(break): implement total and donation popups

--- graphics/elements/molecules/gdq-break/gdq-break-top-frame.js
+++ graphics/elements/molecules/gdq-break/gdq-break-top-frame.js
@@ -1,15 +1,92 @@
-/**
- * @customElement
- * @polymer
- */
-class GdqBreakTopFrame extends Polymer.Element {
-	static get is() {
-		return 'gdq-break-top-frame';
+/* global Random */
+(function () {
+	'use strict';
+
+	/**
+	 * @customElement
+	 * @polymer
+	 */
+	class GdqBreakTopFrame extends Polymer.Element {
+		static get is() {
+			return 'gdq-break-top-frame';
+		}
+
+		ready() {
+			super.ready();
+			this.$.totalTextAmount.displayValueTransform = this._totalDisplayValueTransform.bind(this);
+
+			nodecg.readReplicant('total', totalVal => {
+				this.$.totalTextAmount.value = totalVal.raw;
+				nodecg.listenFor('donation', this._handleDonation.bind(this));
+			});
+
+			nodecg.listenFor('total:manuallyUpdated', totalVal => {
+				this.$.totalTextAmount.value = totalVal.raw;
+			});
+		}
+
+		donationText(formattedAmount, rawAmount) {
+			const div = document.createElement('div');
+			div.classList.add('donationText');
+			div.innerText = formattedAmount;
+
+			if (rawAmount >= 500) {
+				div.style.backgroundColor = '#FF68B9';
+			} else if (rawAmount >= 100) {
+				div.style.backgroundColor = '#FFFBBD';
+			} else if (rawAmount >= 20) {
+				div.style.backgroundColor = '#00ffff';
+			} else {
+				div.style.backgroundColor = 'white';
+			}
+
+			this.$.totalText.appendChild(div);
+			div.style.left = `${randomInt(0, this.$.totalText.clientWidth - div.clientWidth)}px`;
+			div.style.bottom = `${randomInt(2, 8)}px`;
+
+			const tl = new TimelineLite();
+
+			tl.to(div, 0.1834, {
+				clipPath: 'inset(0 0%)',
+				ease: Power1.easeIn
+			});
+
+			tl.addLabel('exit', rawAmount >= 500 ? 1 : 0.067);
+			tl.to(div, 0.934, {
+				y: -21,
+				ease: Power1.easeIn
+			}, 'exit');
+			tl.to(div, 0.5167, {
+				opacity: 0,
+				ease: Power1.easeIn
+			}, 'exit+=0.4167');
+
+			tl.call(() => {
+				div.remove();
+			});
+		}
+
+		_handleDonation({amount, rawAmount, rawNewTotal}) {
+			this.donationText(amount, rawAmount);
+			this.$.totalTextAmount.value = rawNewTotal;
+		}
+
+		_totalDisplayValueTransform(displayValue) {
+			return displayValue.toLocaleString('en-US', {
+				maximumFractionDigits: 0
+			}).replace(/1/ig, '\u00C0');
+		}
 	}
 
-	static get properties() {
-		return {};
-	}
-}
+	customElements.define(GdqBreakTopFrame.is, GdqBreakTopFrame);
 
-customElements.define(GdqBreakTopFrame.is, GdqBreakTopFrame);
+	/**
+	 * Generates a random integer.
+	 * @param {Number} min - The minimum number, inclusive.
+	 * @param {Number} max - The maximmum number, inclusive.
+	 * @returns {Number} - A random number between min and max, inclusive.
+	 */
+	function randomInt(min, max) {
+		return Random.integer(min, max)(Random.engines.browserCrypto);
+	}
+})();
