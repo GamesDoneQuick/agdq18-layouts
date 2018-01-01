0f4fe8c5e83a968e639e8120440b8174ab59c5e6
Alex Van Camp <vancamp.alex@gmail.com>
2018-01-01 09:25:25 -0600

    feat(gdq-lowerthird): implement FUI style (#2)

--- graphics/elements/molecules/gdq-lowerthird/gdq-lowerthird.js
+++ graphics/elements/molecules/gdq-lowerthird/gdq-lowerthird.js
@@ -17,18 +17,16 @@
 						return new TimelineLite({autoRemoveChildren: true});
 					},
 					readOnly: true
-				},
-				names: Array,
-				numNames: {
-					type: Number,
-					reflectToAttribute: true
-				},
-				greebles: Array
+				}
 			};
 		}
 
 		ready() {
 			super.ready();
+
+			this.$.header.updateName({alias: '#AGDQ2018', twitchAlias: null, rotate: false});
+			this._$nameElements = Array.from(this.$.names.querySelectorAll('gdq-nameplate'));
+
 			lowerthirdShowing.on('change', newVal => {
 				if (newVal) {
 					this.show();
@@ -43,81 +41,31 @@
 
 			// Set names
 			tl.call(() => {
-				let hostName;
-				let intervieweeNames = interviewNames.value.slice(0);
-				if (intervieweeNames.length === 5) {
-					hostName = intervieweeNames.shift();
-				}
-				intervieweeNames = intervieweeNames.filter(name => Boolean(name));
-
-				this.style.willChange = 'height';
-				this.$.names.style.willChange = 'height';
-				this.$.dropdown.style.willChange = 'transform';
-				this.names = intervieweeNames;
-				this.numNames = this.names.length;
-
-				const greebles = [];
-				const numGreebles = Math.max(2, this.numNames);
-				for (let i = 0; i < numGreebles; i++) {
-					greebles.push(true);
-				}
-				this.greebles = greebles;
-
-				if (hostName) {
-					this.$['dropdown-middle-text'].innerHTML =
-						`Interviewer:&nbsp;<span style="color: white;">${hostName}</span>`;
-				} else {
-					this.$['dropdown-middle-text'].innerHTML = '#AGDQ2018';
-				}
-
-				Polymer.flush();
+				const names = interviewNames.value.filter(name => Boolean(name));
+				this._$nameElements.forEach((nameElement, index) => {
+					nameElement.updateName({alias: names[index], twitchAlias: null, rotate: false});
+					nameElement.hidden = !names[index];
+				});
 			}, null, null, '+=0.3'); // Give time for interviewNames replicant to update.
 
-			// Fit names
-			tl.call(() => {
-				const padding = this.numNames === 4 ? 24 : 36;
-				const nameDivs = Array.from(this.querySelectorAll('.name'));
-				if (nameDivs.length > 0) {
-					const maxNameWidth = nameDivs[0].parentNode.clientWidth - padding;
-					nameDivs.forEach(nameDiv => {
-						const nameWidth = nameDiv.scrollWidth;
-						if (nameWidth > maxNameWidth) {
-							TweenLite.set(nameDiv, {scaleX: maxNameWidth / nameWidth});
-						} else {
-							TweenLite.set(nameDiv, {scaleX: 1});
-						}
-					});
-				}
-			});
-
-			tl.to(this, 1, {
-				height: 153,
+			tl.to(this, 0.2, {
+				opacity: 1,
 				ease: Power3.easeInOut
 			});
 
-			tl.call(() => {
-				TweenLite.to(this.$.names, 0.65, {
-					height: this.numNames === 4 ? 48 : 65,
-					ease: Power3.easeInOut
-				});
-			}, null, this, '-=0.8');
-
-			tl.call(() => {
-				TweenLite.to(this.$.dropdown, 0.65, {
-					y: this.numNames === 4 ? 41 : 58,
-					ease: Power3.easeOut
-				});
-			}, null, this, '-=0.4');
+			tl.staggerFrom(this._$nameElements, 0.2, {
+				x: -30,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.15);
 		}
 
 		hide() {
 			const tl = this.tl;
-			tl.to(this, 0.9, {
-				height: 0,
+			tl.to(this, 0.4, {
+				opacity: 0,
 				ease: Power3.easeIn
 			});
-
-			tl.set([this, this.$.names, this.$.dropdown], {clearProps: 'all'});
 		}
 	}
 
