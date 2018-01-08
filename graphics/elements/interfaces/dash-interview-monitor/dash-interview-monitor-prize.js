f66cf80f3d1a9d4ce25dfc6dc101057b5ea5d629
Alex Van Camp <vancamp.alex@gmail.com>
2018-01-08 17:55:15 -0500

    feat(dash-interview-monitor): show more info about prizes, such as open and close run

--- graphics/elements/interfaces/dash-interview-monitor/dash-interview-monitor-prize.js
+++ graphics/elements/interfaces/dash-interview-monitor/dash-interview-monitor-prize.js
@@ -1,26 +1,85 @@
-/**
- * @customElement
- * @polymer
- */
-class DashInterviewMonitorPrize extends Polymer.Element {
-	static get is() {
-		return 'dash-interview-monitor-prize';
-	}
+(function () {
+	'use strict';
 
-	static get properties() {
-		return {
-			prize: Object,
-			bidType: {
-				type: String,
-				reflectToAttribute: true,
-				computed: '_computeBidType(prize)'
+	const currentRun = nodecg.Replicant('currentRun');
+
+	/**
+	 * @customElement
+	 * @polymer
+	 */
+	class DashInterviewMonitorPrize extends Polymer.MutableData(Polymer.Element) {
+		static get is() {
+			return 'dash-interview-monitor-prize';
+		}
+
+		static get properties() {
+			return {
+				prize: Object,
+				currentRun: Object,
+				bidType: {
+					type: String,
+					reflectToAttribute: true,
+					computed: '_computeBidType(prize)'
+				},
+				closed: {
+					type: Boolean,
+					reflectToAttribute: true,
+					computed: '_computeClosed(prize, currentRun)'
+				}
+			};
+		}
+
+		ready() {
+			super.ready();
+
+			currentRun.on('change', newVal => {
+				this.currentRun = newVal;
+			});
+		}
+
+		_computeBidType(prize) {
+			return prize.sumdonations ? 'total' : 'single';
+		}
+
+		_computeClosed(prize, currentRun) {
+			if (!prize || !currentRun) {
+				return false;
 			}
-		};
+
+			return prize.endrun.order < currentRun.order;
+		}
+
+		_calcBidTypeChar(bidType) {
+			if (!bidType) {
+				return '';
+			}
+			return bidType.charAt(0);
+		}
+
+		_calcOpening(prize, currentRun) {
+			if (!prize || !currentRun) {
+				return '?';
+			}
+
+			if (prize.startrun.order <= currentRun.order) {
+				return 'OPEN';
+			}
+
+			return prize.startrun.name;
+		}
+
+		_calcClosing(prize, currentRun) {
+			if (!prize || !currentRun) {
+				return '?';
+			}
+
+			if (prize.endrun.order < currentRun.order) {
+				return 'CLOSED';
+			}
+
+			return prize.endrun.name;
+		}
 	}
 
-	_computeBidType(prize) {
-		return prize.sumdonations ? 'total' : 'single';
-	}
-}
-
-customElements.define(DashInterviewMonitorPrize.is, DashInterviewMonitorPrize);
+	customElements.define(DashInterviewMonitorPrize.is, DashInterviewMonitorPrize);
+})();
